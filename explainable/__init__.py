df64dd27871d638706132d3633b0399b86f3b9e3
Jorge Bermudez <jabc.chile@gmail.com>
2022-10-18 14:05:51 -0300

    initital commit

---
+++ explainable/__init__.py
@@ -0,0 +1,11 @@
+from gym.envs.registration import register
+
+register(
+    id='RMSA-v0',
+    entry_point='explainable.envs:RMSAEnv',
+)
+
+register(
+    id='DeepRMSA-v0',
+    entry_point='explainable.envs:DeepRMSAEnv',
+)
---
+++ explainable/envs/__init__.py
@@ -0,0 +1,2 @@
+from explainable.envs.rmsa_env import RMSAEnv
+from explainable.envs.deeprmsa_env import DeepRMSAEnv
